3551442b789ab5da07cb51001cff59506a30a000
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-25 14:33:09 -0700

    done

--- C6_LetsRunSomeJavascript/Starter/app.js
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -1,3 +1,7 @@
-// Your Javascript Code Goes Here
+'use strict';
 
-// Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
+let a = 1;
+let b = 2;
+let c = a + b;
+
+console.log(c);
