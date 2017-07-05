aedb1bef7d00ccd89e0757c0ad85c828c0168b19
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-07-05 15:53:34 -0700

    part2 video complete

--- E4_EventEmitterPart2/Starter/app.js
+++ E4_EventEmitterPart2/Starter/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// const EE = require('events');
+//
+// const emtr = new EE();
+// //'greet' is a magic string, which is bad
+// emtr.on('greet', () =>{console.log('Somewhere, someone said hello.');});
+//
+// emtr.on('greet', () =>{console.log('A greeting occured');});
+//
+// console.log('Hello');
+// emtr.emit('greet');
+
+const eventConfig = require('./config').events;
+console.log(eventConfig);
+const EE = require('events');
+
+const emtr = new EE();
+
+emtr.on(eventConfig.GREET, () =>{console.log('Somewhere, someone said hello.');});
+
+emtr.on(eventConfig.GREET, () =>{console.log('A greeting occured');});
+
+console.log('Hello');
+emtr.emit(eventConfig.GREET);
