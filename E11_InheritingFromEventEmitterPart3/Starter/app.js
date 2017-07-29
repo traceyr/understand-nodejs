24af4417cfaeb8b5bb7c0916c36d5de7c186a726
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-07-29 16:52:38 -0700

    section 6 finished

--- E11_InheritingFromEventEmitterPart3/Starter/app.js
+++ E11_InheritingFromEventEmitterPart3/Starter/app.js
@@ -1,22 +1,39 @@
+'use strict';
+
 var EventEmitter = require('events');
-var util = require('util');
+var util = require('util'); //Dont need with ES6
+//Before ES6
+// function Greetr() {
+//   EventEmitter.call(this);
+//   this.greeting = 'Hello world!';
+// }
+//
+// util.inherits(Greetr, EventEmitter);
+//
+// Greetr.prototype.greet = function(data) {
+//   console.log(this.greeting + ': ' + data);
+//   this.emit('greet', data);
+// };
 
-function Greetr() {
-	EventEmitter.call(this);
-	this.greeting = 'Hello world!';
-}
+//After ES6
 
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-	console.log(this.greeting + ': ' + data);
-	this.emit('greet', data);
-}
+// class Greetr extends EventEmitter {
+//   constructor() {
+//     super();
+//     this.greeting = 'Hello World';
+//   }
+//
+//   greet(data) {
+//     console.log((`${this.greeting}: ${data}`));
+//     this.emit('greet', data);
+//   }
+// }
+let Greetr = require('./greetr');
 
 var greeter1 = new Greetr();
 
 greeter1.on('greet', function(data) {
-	console.log('Someone greeted!: ' + data);
+  console.log('Someone greeted!: ' + data);
 });
 
 greeter1.greet('Tony');
