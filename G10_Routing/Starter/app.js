31e93eb2aaccb728acef85d205e63ba4d4a61857
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-02-26 18:19:37 -0800

    added routing for basic web server

--- G10_Routing/Starter/app.js
+++ G10_Routing/Starter/app.js
@@ -1,13 +1,21 @@
-var http = require('http');
-var fs = require('fs');
+'use strict';
+
+let http = require('http');
+let fs = require('fs');
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var obj = {
-    firstname: 'John',
-    lastname: 'Doe'
-  };
-  res.end(JSON.stringify(obj));
-
-}).listen(8080, '127.0.0.1');
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  } else if (req.url === '/api') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    let obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}).listen(1337, '127.0.0.1');
