f676d07d9b559057122b780076643736af1c608d
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-04-01 17:27:32 -0700

    routing with req.params

--- I2_Routing/Starter/app.js
+++ I2_Routing/Starter/app.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+
+let port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
 	res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
 });
 
+app.get('/person/:id', function(req, res) {
+	res.send('<html><head></head><body><h1>Person: '+ req.params.id + '!</h1></body></html>');
+})
 app.get('/api', function(req, res) {
 	res.json({ firstname: 'John', lastname: 'Doe' });
 });
