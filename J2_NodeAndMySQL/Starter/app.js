1ffbbbed72f45286708609e7ee9d12110dbca5d5
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-04-10 18:07:52 -0700

    added npm mysql db

--- J2_NodeAndMySQL/Starter/app.js
+++ J2_NodeAndMySQL/Starter/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -12,6 +13,30 @@ app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
+
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "test",
+		password: "test",
+		// database: "addressbook"
+	});
+
+	// con.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddresses On People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Address.ID', function(err, rows){
+	// 	if(err) throw err;
+	// 	console.log(rows);
+	// });
+
+	con.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + con.threadId);
+});
+
+con.end()
+
 	next();
 });
 
