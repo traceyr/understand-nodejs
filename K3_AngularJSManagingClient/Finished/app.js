1ff780039339c94c00fc2d79ba94803b3426ac8b
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-04-15 18:47:39 -0700

    made small changes to test app, epsecially to check if ang1 still works

--- K3_AngularJSManagingClient/Finished/app.js
+++ K3_AngularJSManagingClient/Finished/app.js
@@ -7,9 +7,11 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	
+
 	res.render('index');
-	
+
 });
 
-app.listen(port);
+app.listen(port, function() {
+	console.log('App listening on port ' + port);
+});
