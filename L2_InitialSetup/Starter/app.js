e836780d95eae91654d52e24f31aab31c237e1ec
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-04-16 12:07:27 -0700

    added mongo and mongoose db and schema

--- L2_InitialSetup/Starter/app.js
+++ L2_InitialSetup/Starter/app.js
@@ -2,11 +2,15 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+let config = require('./config');
 let port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'esj');
+
+mongoose.connect(config.getDBConnectionString());
 
 app.listen(port, function(){
   console.log(`App listening on port ${port}`);
